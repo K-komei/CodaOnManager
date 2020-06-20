afaf499689a2f266ce5f4a202eacbff262a541ba
K-komei <53886280+K-komei@users.noreply.github.com>
2020-06-20 17:38:12 +0900

    Source修正

---
+++ coda_on_manager/src/main/java/coda_on_manager/coda_on_manager/RootController.java
@@ -0,0 +1,16 @@
+package coda_on_manager.coda_on_manager;
+
+ 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+ 
+ 
+@RestController 
+
+public class RootController {
+    @RequestMapping("/")
+    public String index() {
+        return "Spring Boot Sample!";
+    }
+    
+}
