8fa5560671573ce4de3702c4f027a3fbedbbb221
K-komei <53886280+K-komei@users.noreply.github.com>
2020-07-26 18:01:21 +0900

    FileDB制御

--- coda_on_manager/src/main/java/coda_on_manager/coda_on_manager/controller/CodaController.java
+++ coda_on_manager/src/main/java/coda_on_manager/coda_on_manager/controller/CodaController.java
@@ -4,10 +4,12 @@ import java.lang.ProcessBuilder.Redirect;
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
+import javax.validation.constraints.Null;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.SpringTransactionAnnotationParser;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -33,27 +35,30 @@ public class CodaController {
 
 //DI注入
 @Autowired
-
 //UserDataserviceレポジトリを定義
-
 UserData_repository user_rp;
+
+@Autowired
 CodeData_repository code_rp;
 
 
 @GetMapping("Editor")
-public String Editor(Model model) {
-
+public String Editor(Model model,HttpSession session) {
 CodeData_Entity code = new CodeData_Entity();
 model.addAttribute("code",code);
 
+UserData_Entity sessionuser = (UserData_Entity) session.getAttribute("useid");
+model.addAttribute("user",sessionuser);
 return "Editor";
 }
 
 @PostMapping("Editor")
-public String Editor(Model model,@ModelAttribute UserData_Entity user) {
-CodeData_Entity code = new CodeData_Entity();
-model.addAttribute("code",code);
-return "Editor";
+public String Editor(Model model,HttpSession session,CodeData_Entity code) {
+        
+        model.addAttribute("code",code_rp.findById(code.getId()));
+        UserData_Entity sessionuser = (UserData_Entity) session.getAttribute("useid");
+        model.addAttribute("user",sessionuser);
+        return "Editor";
 }
 
 
@@ -65,23 +70,45 @@ return "SiginUp";
 }
 
 @PostMapping("Upload")
-public String Upload(CodeData_Entity code) {
-    
-        System.out.println(code.getId());
-        System.out.println(code.getCodename());
-        System.out.println(code.getCodebody());
-        System.out.println(code.getCreate_user_id());
+public String Upload(CodeData_Entity code,HttpSession session,Model model) {
+ //sessionよりユーザーデータを取り出す
+ UserData_Entity user = (UserData_Entity) session.getAttribute("useid");
+code.setCreate_user_id(user.getId());
 
-        code_rp.save(code);
-    
-return "redirect:Editor";
+if(code.getCodename().isEmpty()){
+
+        return "redirect:Editor";
+
+}else{
+code_rp.save(code);
+        return "redirect:Editor";
+
+}
+
+
+
+
 
 }
 
 @GetMapping("Filelist")
-public String CodeData_view(Model model){
-List<CodeData_Entity> code_data = code_rp.findAll();
-model.addAttribute("codes",code_data);
+public String CodeData_view(Model model,HttpSession session){        
+
+ //sessionよりユーザーデータを取り出す
+ UserData_Entity user = (UserData_Entity) session.getAttribute("useid");
+
+ //検索条件(Name Pass)を設定したEntityを生成する。
+ CodeData_Entity target_code = new CodeData_Entity();
+ target_code.setCreate_user_id(user.getId());
+
+ // 生成したEntityからExampleインスタンスを生成
+  Example<CodeData_Entity> example = Example.of(target_code);
+  //検索を行い、一致したEntityをListに格納
+
+  List<CodeData_Entity> codes = code_rp.findAll(example);
+
+  model.addAttribute("codes",codes);
+  model.addAttribute("user",user);
 return "FileList";
 }
 
@@ -100,8 +127,8 @@ target_user.setPass(user.getPass());
  // 生成したEntityからExampleインスタンスを生成
  Example<UserData_Entity> example = Example.of(target_user);
 
- //検索を行い、一致したEntityをListに格納
-List<UserData_Entity> users = user_rp.findAll(example);
+  //検索を行い、一致したEntityをListに格納
+ List<UserData_Entity> users = user_rp.findAll(example);
 
 //上で取得したリストが"1件のみ"である場合、一致成功とし、それ以外の場合、認証不可とする。
 
@@ -109,11 +136,8 @@ if(users.size() == 0){
         return "redirect:SiginUp";
 }else{
         UserData_Entity keepdata =users.get(0);
-        //session.setAttribute("id", 1);
-        //System.out.println(session.getId());
-        session.setAttribute("useid",keepdata.getId());
-        return "redirect:Editor";
-
+        session.setAttribute("useid",keepdata);
+        return "redirect:Filelist";
 }
 
 }
